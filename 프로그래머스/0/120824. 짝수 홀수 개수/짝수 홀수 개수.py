b401283870916feb2e92176aa36443176b2b64e9
Doeun <68941249+RKDLDE@users.noreply.github.com>
2026-03-07 17:53:50 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    odd, even = 0, 0
+    for i in num_list:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    answer.append(even)
+    answer.append(odd)
+    return answer
