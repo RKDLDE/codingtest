01bb734253ac5900efcb599904cbc7bf2abd2232
Doeun <68941249+RKDLDE@users.noreply.github.com>
2026-04-27 22:54:16 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,8 +1,10 @@
 def solution(n):
     even, odd = 0, 0
-    for i in range(1, n+1):
-        if i % 2 == 0:
+    if n % 2 == 0:
+        for i in range(2, n+1, 2):
             odd = i * i + odd
-        else:
+        return odd
+    else:
+        for i in range(1, n+1, 2):
             even += i
-    return even if n % 2 != 0 else odd
+        return even
